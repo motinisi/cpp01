64bbd6062be678f7d86b596bc9b86e8e5cb4b673
motinisi <nisi77851@gmail.com>
2025-07-19 17:14:40 +0900

    ex06 fin

---
+++ ex06/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+
+int main(int argc, char **argv) {
+	Harl harl;
+	if (argc != 2) {
+		std::cerr << "Usage: " << argv[0] << " <level>" << std::endl;
+		return 1;
+	}
+	std::string level = argv[1];
+    harl.complain(level);
+    return 0;
+}
